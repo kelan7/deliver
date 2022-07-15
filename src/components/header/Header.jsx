0de929b28e1dc9c69c867eeb547f2d0a13022e8c
Kelan <bsarev.vv@gmail.com>
2022-07-15 12:20:49 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './header.module.css';
+
+
+const Header = () => {
+    
+    return (
+        <div className={classes.wrapper}>
+
+        </div>
+    );
+}
+
+
+export default Header;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './main.module.css';
+
+const Main = () => {
+
+    return (
+        <div className={classes.wrapper}>
+
+        </div>
+    );
+}
+
+
+export default Main;
